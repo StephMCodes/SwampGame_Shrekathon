ef1299fd905b42868215c58f80131c84fd28bdea
Stephanie M <shelovesgames965@gmail.com>
2026-03-21 13:35:53 -0400

    work

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int onionCount = 0;
+    public static bool gameOver = false;
+    [SerializeField] GameObject endingCanvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+           endingCanvas.SetActive(true);
+           Cursor.visible = true;
+           Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
