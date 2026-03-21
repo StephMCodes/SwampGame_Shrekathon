8a7f4ed0dcde739db9a54c06e19999df36d58c60
Stephanie M <shelovesgames965@gmail.com>
2026-03-21 14:07:18 -0400

    npc start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+           Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
