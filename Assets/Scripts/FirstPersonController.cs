3340f8fd86fce3b2767ba7c979de1bb2a5cb23fb
Stephanie M <shelovesgames965@gmail.com>
2026-03-20 20:05:04 -0400

    first person cont.

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+//[SerializeField] DisablePrivateReflectionAttribute float walkSpeed = 3.0f;
+
+public class FirstPersonController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInputHandler : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset playerControls;
+    [SerializeField] private string actionMapName = "Player";
+
+
+    [SerializeField] private string movement = "Movement";
+    [SerializeField] private string rotation = "Rotation";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string sprint = "Sprint";
+
+    private InputAction movementAction;
+    private InputAction rotationAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+
+    public Vector2 MovementInput { get; private set; }
+    public Vector2 RotationInput { get; private set; }
+
+    public bool jumpTrigger { get; private set; }
+    public bool sprintTrigger { get; private set; }
+
+
+    private void Awake()
+    {
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        movementAction = mapReference.FindAction(movement);
+        rotationAction = mapReference.FindAction(rotation);
+        jumpAction = mapReference.FindAction(jump);
+        sprintAction = mapReference.FindAction(sprint);
+
+        SubscribeActionValuesToInputEvents();
+
+
+    }
+
+    private void SubscribeActionValuesToInputEvents()
+    {
+        movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+        movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
+
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
+        rotationAction.canceled += inputInfo => RotationInput = Vector2.zero;
+
+        jumpAction.performed += inputInfo => jumpTrigger = true;
+        jumpAction.canceled += inputInfo => jumpTrigger = false;
+
+        sprintAction.performed += inputInfo => sprintTrigger = true;
+        sprintAction.canceled += inputInfo => sprintTrigger = false;
+
+    }
+
+    private void OnEnable()
+    {
+        playerControls.FindActionMap(actionMapName).Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.FindActionMap(actionMapName).Disable();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
